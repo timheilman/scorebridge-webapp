b675c8a5ef995ae7c54dd6424ea7271f7020542b
Tim Heilman <tdh@stanfordalumni.org>
2023-07-16 17:45:28 -0700

    build: SCOR-54 testing local auto-redeploy on source change and
    aws amplify ci/cd

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Testing local auto-reload and AWS Amplify CI/CD.
         </p>
         <a
           className="App-link"
