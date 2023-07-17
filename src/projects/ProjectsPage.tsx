32706ae43bca697d93f9efe9826c3276aecec860
Tim Heilman <tdh@stanfordalumni.org>
2023-07-17 10:38:29 -0700

    build: SCOR-54 results of https://handsonreact.com/docs/labs/ts/CreatingDataStructures

--- src/projects/ProjectsPage.tsx
+++ src/projects/ProjectsPage.tsx
@@ -1,5 +1,12 @@
+import { MOCK_PROJECTS} from "./MockProjects";
+
 function ProjectsPage() {
-    return <h1>Projects</h1>;
+    return (
+        <>
+            <h1>Projects</h1>
+            <pre>{JSON.stringify(MOCK_PROJECTS, null, ' ')}</pre>
+        </>
+    );
 }
 
 export default ProjectsPage;
