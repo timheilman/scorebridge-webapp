e0bd7844bc9f459fcd0fcb938ef8c2a4680b9a09
Tim Heilman <tdh@stanfordalumni.org>
2023-08-01 12:32:30 -0700

    test: exploratory, passing props and using map/reduce with components
    per https://handsonreact.com/docs/lists

--- src/projects/HelloWorld.tsx
+++ src/projects/HelloWorld.tsx
@@ -1,11 +1,39 @@
-function HelloWorld(props: {
+// per https://handsonreact.com/docs/react-typescript
+export interface Props {
   person: { first: string; last: string };
   logo: { name: string; title: string; path: string };
-}) {
-  const { first, last } = props.person;
-  const { path, title } = props.logo;
+}
+
+type Fruit = {
+  id: number;
+  name: string;
+};
+function FruitListItem(props: { fruit: Fruit }) {
+  return <li>{props.fruit.name}</li>;
+}
+
+function FruitList(props: { fruits: Fruit[] }) {
+  const fruitListItems = props.fruits.map((fruit: Fruit) => (
+    <FruitListItem key={fruit.id} fruit={fruit} />
+    // <FruitListItem fruit={fruit} />
+  ));
+  return <ul>{fruitListItems}</ul>;
+}
+
+const constFruits: Fruit[] = [
+  { id: 1, name: "apple" },
+  { id: 2, name: "orange" },
+  { id: 3, name: "blueberry" },
+  { id: 4, name: "banana" },
+  { id: 5, name: "kiwi" },
+];
+
+function HelloWorld({ person, logo }: Props) {
+  const { first, last } = person;
+  const { path, title } = logo;
   return (
     <>
+      <FruitList fruits={constFruits} />
       <img src={path} alt={title} />
       Hello, {last}, {first}!
       <ul>
