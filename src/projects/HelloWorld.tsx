4110a35d998b1b2cfa185058427695d8026b81ea
Tim Heilman <tdh@stanfordalumni.org>
2023-07-31 18:35:49 -0700

    test: learning, Hello World thru jsx
    per https://handsonreact.com/docs/jsx

--- src/projects/HelloWorld.tsx
+++ src/projects/HelloWorld.tsx
@@ -1,5 +1,28 @@
+const person = { first: "Tim", last: "Heilman" };
+const logo = {
+  name: "Logo",
+  title: "Logo",
+  path: "./logo512.png", // by experiment, . corresponds to /public
+};
 function HelloWorld() {
-  return <>Hello, World!</>;
+  return (
+    <>
+      <img src={logo.path} alt={logo.title} />
+      Hello, {person.last}, {person.first}!
+      <ul>
+        <li>
+          <a href="">a</a>
+        </li>
+        <li>
+          <a href="">b</a>
+        </li>
+        <li>
+          <a href="">c</a>
+        </li>
+      </ul>
+    </>
+  );
+  // return <>Hello, world!</>;
 }
 
 export default HelloWorld;
