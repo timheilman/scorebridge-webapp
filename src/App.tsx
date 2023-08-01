87f7f5cd8a42d01a617e8ec49a6db26b6b6cf166
Tim Heilman <tdh@stanfordalumni.org>
2023-07-31 18:20:08 -0700

    test: learning, Hello World ish from
    https://handsonreact.com/docs/jsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import "./App.css";
 
+import HelloWorld from "./projects/HelloWorld";
 import ProjectsPage from "./projects/ProjectsPage";
 
 function App() {
   return (
     <div className="container">
       <ProjectsPage />
+      <HelloWorld />
     </div>
   );
 }
