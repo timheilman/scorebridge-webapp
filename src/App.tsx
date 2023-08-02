98d258f79b4486dd132c264d899b4b4c1c1c63c7
Tim Heilman <tdh@stanfordalumni.org>
2023-08-02 16:21:18 -0700

    test: exploratory, routing, lab 20, labs 16-19 were skipped
    b/c form validation (16) should use a library and I am using graphQl not REST.  But routing is important

--- src/App.tsx
+++ src/App.tsx
@@ -9,13 +9,43 @@ const logo = {
   path: "./logo512.png", // by experiment, . corresponds to /public
 };
 
-function App() {
+import {
+  BrowserRouter as Router,
+  NavLink,
+  Route,
+  Routes,
+} from "react-router-dom";
+
+import HomePage from "./home/HomePage";
+
+export default function App() {
   return (
-    <div className="container">
-      <ProjectsPage />
-      <HelloWorld person={person} logo={logo} />
-    </div>
+    <Router>
+      <header className="sticky">
+        <span className="logo">
+          <img src="/assets/logo-3.svg" alt="logo" width="49" height="99" />
+        </span>
+        <NavLink to="/" className="button rounded">
+          <span className="icon-home"></span>
+          Home
+        </NavLink>
+        <NavLink to="/projects" className="button rounded">
+          Projects
+        </NavLink>
+        <NavLink to="/helloworld" className="button rounded">
+          Hello World Hands-On-React Examples
+        </NavLink>
+      </header>
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/projects" element={<ProjectsPage />} />
+          <Route
+            path="/helloworld"
+            element={<HelloWorld person={person} logo={logo} />}
+          />
+        </Routes>
+      </div>
+    </Router>
   );
 }
-
-export default App;
