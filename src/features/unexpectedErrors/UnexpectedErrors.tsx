fcd66bb1ea11c207917603f8a0a9980f2d2701cb
Tim Heilman <tdh@stanfordalumni.org>
2023-08-16 10:59:31 -0700

    fix: functional alternation of auth type in unexpectedError

--- src/features/unexpectedErrors/UnexpectedErrors.tsx
+++ src/features/unexpectedErrors/UnexpectedErrors.tsx
@@ -1,21 +1,28 @@
 import { GraphQLQuery } from "@aws-amplify/api";
+import { AuthStatus } from "@aws-amplify/ui";
+import { useAuthenticator } from "@aws-amplify/ui-react";
 import { API, graphqlOperation } from "aws-amplify";
 import { useEffect, useState } from "react";
 
 import { UnexpectedErrorResponse } from "../../../appsync";
 import { mutationUnexpectedError } from "../../graphql/mutations";
 
-const unexpectedErrorCall = async () => {
+const unexpectedErrorCall = async (authStatus: AuthStatus) => {
+  const authMode: { authMode: "AMAZON_COGNITO_USER_POOLS" | "API_KEY" } = {
+    authMode:
+      authStatus === "authenticated" ? "AMAZON_COGNITO_USER_POOLS" : "API_KEY",
+  };
   return API.graphql<GraphQLQuery<UnexpectedErrorResponse>>({
     ...graphqlOperation(mutationUnexpectedError, {}),
-    authMode: "API_KEY",
+    ...authMode,
   });
 };
 
 export default function UnexpectedErrors() {
   const [callResult, setCallResult] = useState<unknown | null>(null);
+  const { authStatus } = useAuthenticator((context) => [context.authStatus]);
   useEffect(() => {
-    unexpectedErrorCall()
+    unexpectedErrorCall(authStatus)
       .then(() => {
         throw new Error(
           "This code is expected never to be reached; this throw indicates a test failure.",
